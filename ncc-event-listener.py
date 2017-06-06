270143706c207d3c0dbd16e68c0f2e8eb78ff231
Einar Nilsen-Nygaard <einarnn@gmail.com>
2017-06-06 15:30:31 +0100

    Simple event listener exxample

---
+++ ncc-event-listener.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+import sys
+from argparse import ArgumentParser
+from ncclient import manager
+from lxml import etree
+import logging
+import time
+import datetime
+from ncclient.transport.session import SessionListener
+
+
+def get(m, filter=None, xpath=None):
+    if filter and len(filter) > 0:
+        return m.get(filter=('subtree', filter))
+    elif xpath and len(xpath)>0:
+        return m.get(filter=('xpath', xpath))
+    else:
+        print ("Need a filter for oper get!")
+        return None
+
+
+if __name__ == '__main__':
+
+    parser = ArgumentParser(description='Select your simple poller parameters:')
+
+    # Input parameters
+    parser.add_argument('--host', type=str, required=True,
+                        help="The device IP or DN")
+    parser.add_argument('-u', '--username', type=str, default='cisco',
+                        help="Go on, guess!")
+    parser.add_argument('-p', '--password', type=str, default='cisco',
+                        help="Yep, this one too! ;-)")
+    parser.add_argument('--port', type=int, default=830,
+                        help="Specify this if you want a non-default port")
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help="Do I really need to explain?")
+
+    # other options
+    parser.add_argument('--stream', type=str, required=True,
+                        help="Event stream to register on")
+
+    args = parser.parse_args()
+
+    if args.verbose:
+        handler = logging.StreamHandler()
+        for l in ['ncclient.transport.ssh', 'ncclient.transport.session', 'ncclient.operations.rpc']:
+            logger = logging.getLogger(l)
+            logger.addHandler(handler)
+            logger.setLevel(logging.DEBUG)
+
+    #
+    # Connect
+    #
+    def unknown_host_cb(host, fingerprint):
+        return True
+    m =  manager.connect(host=args.host,
+                         port=args.port,
+                         username=args.username,
+                         password=args.password,
+                         allow_agent=False,
+                         look_for_keys=False,
+                         hostkey_verify=False,
+                         unknown_host_cb=unknown_host_cb)
+
+    #
+    # create the subscription
+    #
+    s = m.create_subscription(stream_name=args.stream)
+    while True:
+        print('Tick, tock...')
+        n = m.take_notification(timeout=5)
+        if n:
+            print(n)
