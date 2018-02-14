66ad0ca5b6fcb2dd84090150ac9fecb1fce9544e
Einar Nilsen-Nygaard <einarnn@cisco.com>
2018-02-14 12:59:44 +0000

    preparing to use versioneer

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 # Copyright (c) 2018 Cisco and/or its affiliates
 #
 import os
+import versioneer
 from setuptools import setup
 
 __author__ = "Einar Nilsen-Nygaard"
@@ -16,7 +17,8 @@ def read(fname):
 
 setup(
     name='ncc',
-    version='0.1.0',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     description=('A set of NETCONF/YANG-focused scripts using ncclient'),
     long_description="""The package ncc contains a set of scripts for working with NETCONF/YANG via the ncclient library. Scripts support simple tasks such as get/get-config/edit-config and schema download and more! Not all scripts in the repository itself are installed.""",
     # packages = ['ncc'],
